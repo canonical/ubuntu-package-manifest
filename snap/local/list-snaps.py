982c61d5d2fa25f150fe31a713f3b3bb08c709a3
Philip Roche <phil.roche@canonical.com>
2020-10-20 16:14:43 +0100

    Formatting changes thanks to black and flake8

--- snap/local/list-snaps.py
+++ snap/local/list-snaps.py
@@ -6,23 +6,20 @@ import os
 
 DEFAULT_ROOT_DIR = "/"
 
+
 def main(rootdir=DEFAULT_ROOT_DIR):
     installed_snap_details = []
     snap_state_file_path = os.path.join(rootdir, "var/lib/snapd/state.json")
-    with open(snap_state_file_path, 'r') as snap_state_file:
+    with open(snap_state_file_path, "r") as snap_state_file:
         snap_state = json.load(snap_state_file)
-        snap_names = list(snap_state['data']['snaps'].keys())
+        snap_names = list(snap_state["data"]["snaps"].keys())
         snap_names.sort()
         for snap_name in snap_names:
-            snap_info = snap_state['data']['snaps'][snap_name]
-            snap_current_revision = snap_info['current']
-            snap_tracking_channel = snap_info.get('channel', '')
+            snap_info = snap_state["data"]["snaps"][snap_name]
+            snap_current_revision = snap_info["current"]
+            snap_tracking_channel = snap_info.get("channel", "")
             installed_snap_details.append(
-                (
-                    snap_name,
-                    snap_tracking_channel,
-                    snap_current_revision
-                    )
+                (snap_name, snap_tracking_channel, snap_current_revision)
             )
 
     return installed_snap_details
@@ -35,14 +32,17 @@ if __name__ == "__main__":
         dest="rootdir",
         action="store",
         default=DEFAULT_ROOT_DIR,
-        help="Root directory to search for installed snaps (Default is {})".format(
-            DEFAULT_ROOT_DIR
-        ),
+        help="Root directory to search for installed snaps (Default is {})"
+             .format(DEFAULT_ROOT_DIR),
     )
     args = parser.parse_args()
 
     installed_snap_details = main(args.rootdir)
     for installed_snap_detail in installed_snap_details:
         print(
-            "snap:{}\t{}\t{}".format(installed_snap_detail[0], installed_snap_detail[1], installed_snap_detail[2])
+            "snap:{}\t{}\t{}".format(
+                installed_snap_detail[0],
+                installed_snap_detail[1],
+                installed_snap_detail[2],
+            )
         )
